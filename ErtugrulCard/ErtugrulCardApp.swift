19be56c05cdf695e60bf4fba9b25acc9798a8575
Ertuğrul Pancar <21220030023@mersin.edu.tr>
2024-09-03 08:11:08 +0300

    Initial Commit

---
+++ ErtugrulCard/ErtugrulCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ErtugrulCardApp.swift
+//  ErtugrulCard
+//
+//  Created by Ertuğrul Pancar on 3.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct ErtugrulCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
